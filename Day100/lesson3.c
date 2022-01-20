e6762c870bbeb5df75b49dd6d5a9404612e0f85b
DongGeonPark <81406189+DongGeonPark@users.noreply.github.com>
2022-01-20 16:19:48 +0900

    Add files via upload

---
+++ Day100/lesson3.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+
+int main() {
+	/*기본 입출력
+	int num;
+
+	printf("정수를 입력하세요\n");
+
+	scanf_s("%d", &num);
+
+	printf("입력받은 정수: %d", num);
+	*/
+
+	/*문자 입출력
+	char ch;
+
+	printf("문자를입력하세요\n");
+	
+	ch = getchar();
+	printf("%c가 입력되었습니다", ch);
+	*/
+
+
+	/*문제 1
+	int age;
+
+	printf("당신은 몇살입니까?\n");
+	scanf_s("%d", &age);
+	printf("당신은 %d살입니다.", age);
+	*/
+
+	/*문제 2
+	double pi;
+	printf("원주율의 값은?\n");
+	scanf_s("%lf", &pi);
+	printf("원주율의 값은 %lf입니다", pi);
+	*/
+
+	/*문제 3
+
+	char ch;
+	
+	printf("알파벳의 첫 문자는 무엇입니까?\n");
+	ch = getchar();
+	printf("알파벳의 첫 문자는 %c입니다", ch);
+	*/
+
+	/*문제 4
+	float h,w;
+	printf("키는 몇인가요?\n");
+	scanf_s("%f", &h);
+	printf("몸무게는 몇인가요?\n");
+	scanf_s("%f", &w);
+	printf("키는 %0.1f입니다\n", h);
+	printf("몸무게는%0.1f입니다.\n", w);
+	*/
+
+	/*문제 5
+	float h, w;
+	printf("키와 몸무게를 입력하세요\n");
+	scanf_s("%f", &h);
+	scanf_s("%f", &w);
+	printf("키는 %0.1f, 몸무게는 %0.1f\n", h, w);
+	*/
+
+	return 0;
+}
